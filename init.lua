5b38ddd4ababb6a657ceeee4ec33d88a95eff383
Robert Markowski <robert.markowski123@gmail.com>
2023-12-12 07:40:25 +0100

    Deactivate search highlighting

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_exec('language en_US', true)
 vim.api.nvim_exec('set notimeout', true)
+vim.api.nvim_exec('set nohlsearch', true)
 require('remaps')
 require('plugins')
 vim.o.textwidth = 88
