660266d16d81d80a2811921f7e3db895cbbe8f3f
Robert Markowski <robert.markowski123@gmail.com>
2023-12-23 09:45:04 +0100

    Register jump on exiting insert mode

--- lua/remaps/vscode_jumps.lua
+++ lua/remaps/vscode_jumps.lua
@@ -1,66 +1,20 @@
 require('remaps.vscode_utils')
 
-vim.keymap.set({'n'}, 'i',
-    function()
-        register_jump()
-        nvim_feedkeys('i')
-    end
-)
-
-vim.keymap.set({'n'}, 'I',
-    function()
-        register_jump()
-        nvim_feedkeys('I')
-    end
-)
-
-vim.keymap.set({'n'}, 'a',
-    function()
-        register_jump()
-        nvim_feedkeys('a')
-    end
-)
-
-vim.keymap.set({'n'}, 'A',
-    function()
-        register_jump()
-        nvim_feedkeys('A')
-    end
-)
-
-vim.keymap.set({'n'}, 'o',
-    function()
-        register_jump()
-        nvim_feedkeys('o')
-    end
-)
-
-vim.keymap.set({'n'}, 'O',
-    function()
-        register_jump()
-        nvim_feedkeys('O')
-    end
-)
-
-vim.keymap.set({'n'}, 's',
+vim.keymap.set({'n'}, 'x',
     function()
         register_jump()
         if vim.v.count > 0 then
-            nvim_feedkeys(string.format('%ds', vim.v.count))
+            nvim_feedkeys(string.format('%dx', vim.v.count))
         else
-            nvim_feedkeys('s')
+            nvim_feedkeys('x')
         end
     end
 )
 
-vim.keymap.set({'n'}, 'S',
+vim.keymap.set({'i'}, '<escape>',
     function()
+        nvim_feedkeys('<escape>')
         register_jump()
-        if vim.v.count > 0 then
-            nvim_feedkeys(string.format('%dS', vim.v.count))
-        else
-            nvim_feedkeys('S')
-        end
     end
 )
 
@@ -92,14 +46,14 @@ vim.keymap.set({'n'}, '*',
     end
 )
 
-vim.keymap.set({'n'}, 'gg',
+vim.keymap.set({'n', 'v'}, 'gg',
     function()
         register_jump()
         nvim_feedkeys('gg^')
     end
 )
 
-vim.keymap.set({'n'}, 'G',
+vim.keymap.set({'n', 'v'}, 'G',
     function()
         register_jump()
         nvim_feedkeys('G$')
