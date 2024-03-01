3fadad7f85c8d2fb6f0573c514b3202acab4deca
Robert Markowski <robert.markowski123@gmail.com>
2024-03-01 18:43:51 +0100

    Improve default search

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ require('plugins')
 vim.api.nvim_exec('language en_US', true)
 vim.api.nvim_exec('set notimeout', true)
 vim.api.nvim_exec('set nohlsearch', true)
+vim.api.nvim_exec('set ignorecase', true)
+vim.api.nvim_exec('set smartcase', true)
 vim.o.textwidth = 88
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
