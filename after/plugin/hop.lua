b005589b31ee17652ac56962a640e6f4f65df4fc
Robert Markowski <robert.markowski123@gmail.com>
2024-03-09 16:43:44 +0100

    Activate keymaps in all modes

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -15,37 +15,43 @@ vim.keymap.set('', 'T', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, {remap=true})
 
-vim.keymap.set({'n'}, '<leader>l',
+vim.keymap.set('', '<leader>l',
     function()
         hop.hint_lines_skip_whitespace()
     end
 )
 
-vim.keymap.set({'n'}, '<leader>c',
+vim.keymap.set('', ',',
+    function()
+        hop.hint_lines()
+    end
+)
+
+vim.keymap.set('', '<leader>c',
     function()
         hop.hint_char1()
     end
 )
 
-vim.keymap.set({'n'}, '<leader>jd',
+vim.keymap.set('', '<leader>jd',
     function()
         hop.hint_char2()
     end
 )
 
-vim.keymap.set({'n'}, '<leader>w',
+vim.keymap.set('', '<leader>w',
     function()
         hop.hint_words()
     end
 )
 
-vim.keymap.set({'n'}, '<leader>jp',
+vim.keymap.set('', '<leader>jp',
     function()
         hop.hint_patterns()
     end
 )
 
-vim.keymap.set({'n'}, '<leader>ja',
+vim.keymap.set('', '<leader>ja',
     function()
         hop.hint_anywhere()
     end
