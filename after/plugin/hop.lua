03250a412b758d301f2dc7475c10227cfa414fd6
Robert Markowski <robert.markowski123@gmail.com>
2024-03-11 10:04:40 +0100

    Only use hop shortcuts in normal mode to prevent freeze

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -4,21 +4,21 @@ local position = require('hop.hint').HintPosition
 
 hop.setup({ keys = 'kdslaeiowcpnbtyhgruvmfj'})
 
-vim.keymap.set('', 'f',
+vim.keymap.set('n', 'f',
     function()
         hop.hint_char1({
             direction = directions.AFTER_CURSOR,
             current_line_only = true })
     end, {remap=true})
 
-vim.keymap.set('', 'F',
+vim.keymap.set('n', 'F',
     function()
         hop.hint_char1({
             direction = directions.BEFORE_CURSOR,
             current_line_only = true })
     end, {remap=true})
 
-vim.keymap.set('', 't',
+vim.keymap.set('n', 't',
     function()
         hop.hint_char1({
             direction = directions.AFTER_CURSOR,
@@ -26,7 +26,7 @@ vim.keymap.set('', 't',
             hint_offset = -1 })
     end, {remap=true})
 
-vim.keymap.set('', 'T',
+vim.keymap.set('n', 'T',
     function()
         hop.hint_char1({
             direction = directions.BEFORE_CURSOR,
@@ -34,7 +34,7 @@ vim.keymap.set('', 'T',
             hint_offset = 1 })
     end, {remap=true})
 
-vim.keymap.set('', '<leader>e',
+vim.keymap.set('n', '<leader>e',
     function()
         hop.hint_words({
             direction = directions.AFTER_CURSOR,
@@ -42,56 +42,56 @@ vim.keymap.set('', '<leader>e',
             hint_position = position.END })
     end, {remap=true})
 
-vim.keymap.set('', '<leader>w',
+vim.keymap.set('n', '<leader>w',
     function()
         hop.hint_words({
             direction = directions.AFTER_CURSOR,
             current_line_only = true })
     end, {remap=true})
 
-vim.keymap.set('', '<leader>jl',
+vim.keymap.set('n', '<leader>jl',
     function()
         hop.hint_lines_skip_whitespace()
     end
 )
 
-vim.keymap.set('', '<leader>js',
+vim.keymap.set('n', '<leader>js',
     function()
         hop.hint_lines()
     end
 )
 
-vim.keymap.set('', '<leader>jc',
+vim.keymap.set('n', '<leader>jc',
     function()
         hop.hint_char1()
     end
 )
 
-vim.keymap.set('', '<leader>jd',
+vim.keymap.set('n', '<leader>jd',
     function()
         hop.hint_char2()
     end
 )
 
-vim.keymap.set('', ',',
+vim.keymap.set('n', ',',
     function()
         hop.hint_words()
     end
 )
 
-vim.keymap.set('', '<leader>,',
+vim.keymap.set('n', '<leader>,',
     function()
         hop.hint_words({ hint_position = position.END })
     end
 )
 
-vim.keymap.set('', '<leader>jp',
+vim.keymap.set('n', '<leader>jp',
     function()
         hop.hint_patterns()
     end
 )
 
-vim.keymap.set('', '<leader>ja',
+vim.keymap.set('n', '<leader>ja',
     function()
         hop.hint_anywhere()
     end
