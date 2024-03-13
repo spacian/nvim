0aa849591052653218ba216bf555aea435e1e571
Markowski, Robert <robert.markowski@dlr.de>
2024-03-13 18:59:12 +0100

    Add jumps on delete and replace

--- lua/remaps/vscode/jumps.lua
+++ lua/remaps/vscode/jumps.lua
@@ -1,12 +1,49 @@
 require('remaps.vscode.utils')
 
-vim.keymap.set({'n'}, 'x',
+vim.keymap.set({'n'}, 'd',
     function()
         register_jump()
+        register_jump(1)
         if vim.v.count > 0 then
-            nvim_feedkeys(string.format('%dx', vim.v.count))
+            nvim_feedkeys(string.format('%dd', vim.v.count))
         else
-            nvim_feedkeys('x')
+            nvim_feedkeys('d')
+        end
+    end
+)
+
+vim.keymap.set({'n'}, 'D',
+    function()
+        register_jump()
+        register_jump(1)
+        if vim.v.count > 0 then
+            nvim_feedkeys(string.format('%dD', vim.v.count))
+        else
+            nvim_feedkeys('D')
+        end
+    end
+)
+
+vim.keymap.set({'n'}, 'r',
+    function()
+        register_jump()
+        register_jump(1)
+        if vim.v.count > 0 then
+            nvim_feedkeys(string.format('%dr', vim.v.count))
+        else
+            nvim_feedkeys('r')
+        end
+    end
+)
+
+vim.keymap.set({'n'}, 'R',
+    function()
+        register_jump()
+        register_jump(1)
+        if vim.v.count > 0 then
+            nvim_feedkeys(string.format('%dR', vim.v.count))
+        else
+            nvim_feedkeys('R')
         end
     end
 )
