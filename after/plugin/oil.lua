74f64ba483775fdd4a8b07267d39bc1dfe1594a3
Markowski, Robert <robert.markowski@dlr.de>
2024-03-19 10:06:49 +0100

    Add change directory command

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,5 +1,6 @@
 if not vim.g.vscode then
-    require("oil").setup({
+    oil = require("oil")
+    oil.setup({
         keymaps = {
             ["<enter>"] = "actions.select",
             ["<c-p>"] = "actions.preview",
@@ -9,6 +10,16 @@ if not vim.g.vscode then
     })
     vim.keymap.set("n", "<leader>oe",
         function()
-        vim.cmd("silent e %:h")
+            vim.cmd("silent e %:h")
+        end, { noremap = true })
+    vim.keymap.set("n", "<leader>cd",
+        function()
+            local cwd = oil.get_current_dir()
+            if cwd ~= nil then
+                vim.cmd("cd " .. cwd)
+            else
+                vim.cmd("cd %:p:h")
+            end
+            print("current working directory: "..vim.fn.getcwd())
         end, { noremap = true })
 end
