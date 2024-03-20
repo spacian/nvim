9c8a98dec1384c92082575cb87d621de22d8944d
Markowski, Robert <robert.markowski@dlr.de>
2024-03-20 10:57:59 +0100

    Fix colorcolumn, add timeout

--- init.lua
+++ init.lua
@@ -3,12 +3,12 @@ require('plugins')
 if not vim.g.vscode then
     require('autocmd')
     vim.api.nvim_exec('set spell', true)
+    vim.api.nvim_exec('set colorcolumn=+1', true)
 end
 if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.api.nvim_exec('language en_US', true)
-else
-    set colorcolumn=+1
 end
+vim.api.nvim_exec('set timeoutlen=2250', true)
 vim.api.nvim_exec('set nohlsearch', true)
 vim.api.nvim_exec('set ignorecase', true)
 vim.api.nvim_exec('set smartcase', true)
