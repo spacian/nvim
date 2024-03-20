c02505a4fa4360ce0891edfc0ba4cd30bd63b633
spacian <=>
2024-03-20 11:33:37 +0100

    Fix grapple shortcut

--- after/plugin/grapple.lua
+++ after/plugin/grapple.lua
@@ -1,7 +1,7 @@
 if not vim.g.vscode then
     local grapple = require("grapple")
     grapple.setup({ icons = false })
-    vim.keymap.set('n', '<leader>a', function() vim.cmd("silent Grapple tags") end)
+    vim.keymap.set('n', '<leader>a', function() vim.cmd("silent Grapple tag") end)
     vim.keymap.set('n', '<leader>oh', function() require("grapple").open_tags() end)
     vim.keymap.set('n', '<c-n>', function() grapple.cycle("forward") end)
     vim.keymap.set('n', '<c-p>', function() grapple.cycle("backward") end)
