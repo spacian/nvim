8fecfe0b63c4177e54bde813ab7714b590ed1ce4
Markowski, Robert <robert.markowski@dlr.de>
2024-03-21 17:45:38 +0100

    Update grapple shortcuts

--- after/plugin/grapple.lua
+++ after/plugin/grapple.lua
@@ -1,11 +1,10 @@
 if not vim.g.vscode then
     local grapple = require("grapple")
     grapple.setup({ icons = false })
-    vim.keymap.set('n', '<leader>a', function() vim.cmd("silent Grapple tag") end)
+    vim.keymap.set('n', '<leader>a', function() vim.cmd("Grapple tag") end)
     vim.keymap.set('n', '<leader>oh', function() require("grapple").open_tags() end)
-    vim.keymap.set('n', '<c-n>', function() grapple.cycle("forward") end)
-    vim.keymap.set('n', '<c-p>', function() grapple.cycle("backward") end)
-    vim.keymap.set('n', '<c-j>', ":Grapple select index=1<enter>")
-    vim.keymap.set('n', '<c-k>', ":Grapple select index=2<enter>")
-    vim.keymap.set('n', '<c-l>', ":Grapple select index=3<enter>")
+    vim.keymap.set('n', '<leader>h', ":Grapple select index=1<enter>")
+    vim.keymap.set('n', '<leader>j', ":Grapple select index=2<enter>")
+    vim.keymap.set('n', '<leader>k', ":Grapple select index=3<enter>")
+    vim.keymap.set('n', '<leader>l', ":Grapple select index=4<enter>")
 end
