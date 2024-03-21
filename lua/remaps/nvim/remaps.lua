6de554ceedac96b1a2462d6771b990a15e172da2
spacian <=>
2024-03-21 12:04:17 +0100

    Fix navigation mapping

--- lua/remaps/nvim/remaps.lua
+++ lua/remaps/nvim/remaps.lua
@@ -2,11 +2,11 @@ vim.keymap.set({'n'}, '<enter>' , 'o.<backspace><esc>', {noremap=true})
 vim.keymap.set({'n'}, 'o', 'o.<backspace>', {noremap=true})
 vim.keymap.set({'n'}, 'O', 'O.<backspace>', {noremap=true})
 vim.keymap.set({'n'}, '<c-s>', ':w<enter>', {noremap=true})
-vim.keymap.set({'t'}, '<a-q>', [[<c-\><c-n>:q<enter>]], {noremap=true})
-vim.keymap.set('', '<a-h>', [[<c-\><c-n><c-w>h]], {noremap=true})
-vim.keymap.set('', '<a-j>', [[<c-\><c-n><c-w>j]], {noremap=true})
-vim.keymap.set('', '<a-k>', [[<c-\><c-n><c-w>k]], {noremap=true})
-vim.keymap.set('', '<a-l>', [[<c-\><c-n><c-w>l]], {noremap=true})
+vim.keymap.set({'', 'l', 't'}, '<a-q>', [[<c-\><c-n>:q<enter>]], {noremap=true})
+vim.keymap.set({'', 'l', 't'}, '<a-h>', [[<c-\><c-n><c-w>h]], {noremap=true})
+vim.keymap.set({'', 'l', 't'}, '<a-j>', [[<c-\><c-n><c-w>j]], {noremap=true})
+vim.keymap.set({'', 'l', 't'}, '<a-k>', [[<c-\><c-n><c-w>k]], {noremap=true})
+vim.keymap.set({'', 'l', 't'}, '<a-l>', [[<c-\><c-n><c-w>l]], {noremap=true})
 vim.keymap.set(
     {'t'},
     '<a-e>',
