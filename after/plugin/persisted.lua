183838709720d12bff5506b33e17cbe0f4ae0d7d
Robert Markowski <robert.markowski123@gmail.com>
2024-03-22 19:00:55 +0100

    Improve persisted behavior

--- after/plugin/persisted.lua
+++ after/plugin/persisted.lua
@@ -1,12 +1,18 @@
 if not vim.g.vscode then
     require("persisted").setup({
-        autosave = true,
+        autosave = false,
         silent = true,
         ignored_dirs = {"oil:///"},
     })
     require("telescope").load_extension("persisted")
     vim.keymap.set({"n"}, "<leader>ow", ":Telescope persisted<enter>")
-    vim.keymap.set({"n"}, "<leader>sw", 
+    vim.keymap.set({"n"}, "<leader>sw",
         function() require("persisted").save() end
     )
+    vim.api.nvim_create_autocmd({ "VimLeavePre", "BufRead" }, {
+        pattern = {"*"},
+        callback = function()
+            require("persisted").save({force = true})
+        end,
+    })
 end
