faedd706d9ec1a7a344f0ece6e15c5da8a729381
Robert Markowski <robert.markowski123@gmail.com>
2024-03-22 16:25:50 +0100

    Fix persisted oil interaction

--- after/plugin/persisted.lua
+++ after/plugin/persisted.lua
@@ -2,7 +2,11 @@ if not vim.g.vscode then
     require("persisted").setup({
         autosave = true,
         silent = true,
+        ignored_dirs = {"oil:///"},
     })
     require("telescope").load_extension("persisted")
-    vim.keymap.set({"n"}, "<leader>ow", ":Telescope persisted<enter>", {noremap=true})
+    vim.keymap.set({"n"}, "<leader>ow", ":Telescope persisted<enter>")
+    vim.keymap.set({"n"}, "<leader>sw", 
+        function() require("persisted").save() end
+    )
 end
