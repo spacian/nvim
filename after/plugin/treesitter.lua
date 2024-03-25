f672e774f1bf5404fade98da84f608989d847a8e
Markowski, Robert <robert.markowski@dlr.de>
2024-03-25 10:33:21 +0100

    Disable syntax highlighting in vscode

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
   highlight = {
-    enable = true,
+    enable = not vim.g.vscode,
     additional_vim_regex_highlighting = false,
     disable = { "latex" },
   },
