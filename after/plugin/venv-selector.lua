485857774f0d0dcb75410d75dfd405ebd20467b1
Markowski, Robert <robert.markowski@dlr.de>
2024-03-27 13:55:31 +0100

    Improve python workflow

--- after/plugin/venv-selector.lua
+++ after/plugin/venv-selector.lua
@@ -1,17 +1,14 @@
 if not vim.g.vscode then
     require('venv-selector').setup({
         name = {".venv", "venv"},
-        search_workspace = false,
+        search_workspace = true,
         search_venv_managers = false,
         search = true,
         parents = 0,
+        notify_user_on_activate = false,
     })
     vim.keymap.set('n', '<leader>vs', function() vim.cmd('VenvSelect') end)
-    vim.keymap.set('n', '<leader>vc', function() vim.cmd('VenvSelectCached') end)
-    vim.api.nvim_create_autocmd({ "VimEnter", "SessionLoadPost" }, {
-        pattern = {"*"},
-        callback = function()
-            vim.cmd("silent VenvSelectCached")
-        end,
-    })
+    vim.keymap.set('n', '<leader>vc', function()
+        print(require('venv-selector').get_active_venv())
+    end)
 end
