e88a1bee296491f4b04bd85458ce25e6acd96c82
Markowski, Robert <robert.markowski@dlr.de>
2024-03-27 13:38:25 +0100

    Reduce fast access grapples to 3

--- after/plugin/grapple.lua
+++ after/plugin/grapple.lua
@@ -3,8 +3,7 @@ if not vim.g.vscode then
     grapple.setup({ icons = false })
     vim.keymap.set('n', '<leader>a', function() vim.cmd("Grapple tag") end)
     vim.keymap.set('n', '<leader>oh', function() require("grapple").open_tags() end)
-    vim.keymap.set('n', '<leader>h', ":Grapple select index=1<enter>")
-    vim.keymap.set('n', '<leader>j', ":Grapple select index=2<enter>")
-    vim.keymap.set('n', '<leader>k', ":Grapple select index=3<enter>")
-    vim.keymap.set('n', '<leader>l', ":Grapple select index=4<enter>")
+    vim.keymap.set('n', '<leader>j', ":Grapple select index=1<enter>")
+    vim.keymap.set('n', '<leader>k', ":Grapple select index=2<enter>")
+    vim.keymap.set('n', '<leader>l', ":Grapple select index=3<enter>")
 end
