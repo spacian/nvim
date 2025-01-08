7588225d74b7c3918dd211f4254beed9ee6290fc
Markowski, Robert <robert.markowski@dlr.de>
2025-01-08 14:57:53 +0100

    feat: update venv selector and behavior

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -112,8 +112,11 @@ require("lazy").setup({
         dependencies = {
             'neovim/nvim-lspconfig',
             'nvim-telescope/telescope.nvim',
+            'mfussenegger/nvim-dap',
             'mfussenegger/nvim-dap-python',
         },
+        lazy = false,
+        branch = "regexp",
         enabled = not vim.g.vscode,
     },
     {
