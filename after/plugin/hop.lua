fb4317f5bdad4d7188592fe025c0eeb8b00098b8
Markowski, Robert <robert.markowski@dlr.de>
2025-01-08 09:39:23 +0100

    chore: remove unnecessary variables

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -1,15 +1,10 @@
 local hop = require('hop')
-local directions = require('hop.hint').HintDirection
-local position = require('hop.hint').HintPosition
-
 hop.setup({ keys = 'kdslaeiowcpnbtyhgruvmfj'})
-
 vim.keymap.set({'n', 'v'}, '<leader>,',
     function()
         hop.hint_char2()
     end
 )
-
 vim.keymap.set({'n', 'v'}, '<leader>;',
     function()
         hop.hint_camel_case()
