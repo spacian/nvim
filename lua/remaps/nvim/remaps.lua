50a711d47f9f28814c57118ebb0ccaa705d08d91
Markowski, Robert <robert.markowski@dlr.de>
2025-01-09 11:35:47 +0100

    feat: improve mark usage

--- lua/remaps/nvim/remaps.lua
+++ lua/remaps/nvim/remaps.lua
@@ -37,3 +37,11 @@ else
         { noremap = true }
     )
 end
+
+local letters = "abcdefghijklmnopqrstuvwxyz"
+for i = 1, #letters do
+    vim.keymap.set({ 'n' }, 'M' .. letters:sub(i, i):upper(), 'm' .. letters:sub(i, i), { noremap = true })
+    vim.keymap.set({ 'n' }, 'm' .. letters:sub(i, i):upper(), '`' .. letters:sub(i, i), { noremap = true })
+    vim.keymap.set({ 'n' }, 'M' .. letters:sub(i, i), 'm' .. letters:sub(i, i):upper(), { noremap = true })
+    vim.keymap.set({ 'n' }, 'm' .. letters:sub(i, i), '`' .. letters:sub(i, i):upper(), { noremap = true })
+end
