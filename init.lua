0d2999fb4988c27075a4581ccc933810f4b4115c
Markowski, Robert <robert.markowski@dlr.de>
2025-01-09 11:35:01 +0100

    chore: replace deprecated functions

--- init.lua
+++ init.lua
@@ -2,24 +2,24 @@ require('remaps')
 require('plugins')
 if not vim.g.vscode then
     require('autocmd')
-    vim.api.nvim_exec('set spell', true)
-    vim.api.nvim_exec('set colorcolumn=+1', true)
+    vim.cmd('set spell')
+    vim.cmd('set colorcolumn=+1')
     vim.opt.number = true
     vim.opt.relativenumber = false
     vim.opt.cursorline = true
 end
 if vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.api.nvim_exec('language en_US', true)
-    vim.api.nvim_exec('set shell=cmd.exe', true)
+    vim.cmd('language en_US')
+    vim.cmd('set shell=cmd.exe')
 else
-    vim.api.nvim_exec('set wildignorecase', true)
+    vim.cmd('set wildignorecase')
 end
-vim.api.nvim_exec('set shada="None"', true)
-vim.api.nvim_exec('set splitright', true)
-vim.api.nvim_exec('set timeoutlen=2250', true)
-vim.api.nvim_exec('set nohlsearch', true)
-vim.api.nvim_exec('set ignorecase', true)
-vim.api.nvim_exec('set smartcase', true)
+vim.cmd('set shada="None"')
+vim.cmd('set splitright')
+vim.cmd('set timeoutlen=2250')
+vim.cmd('set nohlsearch')
+vim.cmd('set ignorecase')
+vim.cmd('set smartcase')
 vim.o.textwidth = 88
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
