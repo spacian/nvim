e6b0e97515a6bcd0ebfb93a36e09d8f19710e037
Markowski, Robert <robert.markowski@dlr.de>
2025-01-20 13:41:42 +0100

    chore: do not load unused colorschemes

--- after/plugin/nvim/colorscheme.lua
+++ after/plugin/nvim/colorscheme.lua
@@ -1,6 +1,7 @@
 if not vim.g.vscode then
 	local kanagawa = require("kanagawa")
 	kanagawa.setup({
+		compile = true,
 		commentStyle = {},
 		functionStyle = {},
 		keywordStyle = { italic = false },
@@ -28,144 +29,144 @@ if not vim.g.vscode then
 		},
 	})
 
-	require("solarized-osaka").setup({
-		transparent = false,
-		terminal_colors = true,
-		styles = {
-			comments = { italic = false },
-			keywords = { italic = false },
-			functions = {},
-			variables = {},
-			sidebars = "dark",
-			floats = "dark",
-		},
-		sidebars = { "qf", "help" },
-		day_brightness = 0.3,
-		hide_inactive_statusline = false,
-		dim_inactive = false,
-		lualine_bold = false,
-		on_colors = function(colors) end,
-		on_highlights = function(highlights, colors) end,
-	})
+	-- require("solarized-osaka").setup({
+	-- 	transparent = false,
+	-- 	terminal_colors = true,
+	-- 	styles = {
+	-- 		comments = { italic = false },
+	-- 		keywords = { italic = false },
+	-- 		functions = {},
+	-- 		variables = {},
+	-- 		sidebars = "dark",
+	-- 		floats = "dark",
+	-- 	},
+	-- 	sidebars = { "qf", "help" },
+	-- 	day_brightness = 0.3,
+	-- 	hide_inactive_statusline = false,
+	-- 	dim_inactive = false,
+	-- 	lualine_bold = false,
+	-- 	on_colors = function(colors) end,
+	-- 	on_highlights = function(highlights, colors) end,
+	-- })
 
-	require("catppuccin").setup({
-		flavour = "mocha",
-		background = {
-			light = "latte",
-			dark = "mocha",
-		},
-		transparent_background = false,
-		show_end_of_buffer = false,
-		term_colors = true,
-		dim_inactive = {
-			enabled = false,
-			shade = "dark",
-			percentage = 0.15,
-		},
-		no_italic = true,
-		no_bold = true,
-		no_underline = false,
-		styles = {
-			comments = { "italic" },
-			conditionals = { "italic" },
-			loops = {},
-			functions = {},
-			keywords = {},
-			strings = {},
-			variables = {},
-			numbers = {},
-			booleans = {},
-			properties = {},
-			types = {},
-			operators = {},
-		},
-		color_overrides = {},
-		custom_highlights = {},
-		integrations = {
-			cmp = true,
-			gitsigns = true,
-			nvimtree = true,
-			treesitter = true,
-			notify = false,
-			mini = {
-				enabled = true,
-				indentscope_color = "",
-			},
-		},
-	})
+	-- require("catppuccin").setup({
+	-- 	flavour = "mocha",
+	-- 	background = {
+	-- 		light = "latte",
+	-- 		dark = "mocha",
+	-- 	},
+	-- 	transparent_background = false,
+	-- 	show_end_of_buffer = false,
+	-- 	term_colors = true,
+	-- 	dim_inactive = {
+	-- 		enabled = false,
+	-- 		shade = "dark",
+	-- 		percentage = 0.15,
+	-- 	},
+	-- 	no_italic = true,
+	-- 	no_bold = true,
+	-- 	no_underline = false,
+	-- 	styles = {
+	-- 		comments = { "italic" },
+	-- 		conditionals = { "italic" },
+	-- 		loops = {},
+	-- 		functions = {},
+	-- 		keywords = {},
+	-- 		strings = {},
+	-- 		variables = {},
+	-- 		numbers = {},
+	-- 		booleans = {},
+	-- 		properties = {},
+	-- 		types = {},
+	-- 		operators = {},
+	-- 	},
+	-- 	color_overrides = {},
+	-- 	custom_highlights = {},
+	-- 	integrations = {
+	-- 		cmp = true,
+	-- 		gitsigns = true,
+	-- 		nvimtree = true,
+	-- 		treesitter = true,
+	-- 		notify = false,
+	-- 		mini = {
+	-- 			enabled = true,
+	-- 			indentscope_color = "",
+	-- 		},
+	-- 	},
+	-- })
 
-	require("rose-pine").setup({
-		variant = "auto",
-		dark_variant = "main",
-		dim_inactive_windows = false,
-		extend_background_behind_borders = true,
-		enable = {
-			terminal = true,
-			legacy_highlights = true,
-			migrations = true,
-		},
-		styles = {
-			bold = false,
-			italic = false,
-			transparency = false,
-		},
-		groups = {
-			border = "muted",
-			link = "iris",
-			panel = "surface",
-			error = "love",
-			hint = "iris",
-			info = "foam",
-			note = "pine",
-			todo = "rose",
-			warn = "gold",
-			git_add = "foam",
-			git_change = "rose",
-			git_delete = "love",
-			git_dirty = "rose",
-			git_ignore = "muted",
-			git_merge = "iris",
-			git_rename = "pine",
-			git_stage = "iris",
-			git_text = "rose",
-			git_untracked = "subtle",
-			h1 = "iris",
-			h2 = "foam",
-			h3 = "rose",
-			h4 = "gold",
-			h5 = "pine",
-			h6 = "foam",
-		},
-		highlight_groups = {},
-		before_highlight = function(group, highlight, palette) end,
-	})
+	-- require("rose-pine").setup({
+	-- 	variant = "auto",
+	-- 	dark_variant = "main",
+	-- 	dim_inactive_windows = false,
+	-- 	extend_background_behind_borders = true,
+	-- 	enable = {
+	-- 		terminal = true,
+	-- 		legacy_highlights = true,
+	-- 		migrations = true,
+	-- 	},
+	-- 	styles = {
+	-- 		bold = false,
+	-- 		italic = false,
+	-- 		transparency = false,
+	-- 	},
+	-- 	groups = {
+	-- 		border = "muted",
+	-- 		link = "iris",
+	-- 		panel = "surface",
+	-- 		error = "love",
+	-- 		hint = "iris",
+	-- 		info = "foam",
+	-- 		note = "pine",
+	-- 		todo = "rose",
+	-- 		warn = "gold",
+	-- 		git_add = "foam",
+	-- 		git_change = "rose",
+	-- 		git_delete = "love",
+	-- 		git_dirty = "rose",
+	-- 		git_ignore = "muted",
+	-- 		git_merge = "iris",
+	-- 		git_rename = "pine",
+	-- 		git_stage = "iris",
+	-- 		git_text = "rose",
+	-- 		git_untracked = "subtle",
+	-- 		h1 = "iris",
+	-- 		h2 = "foam",
+	-- 		h3 = "rose",
+	-- 		h4 = "gold",
+	-- 		h5 = "pine",
+	-- 		h6 = "foam",
+	-- 	},
+	-- 	highlight_groups = {},
+	-- 	before_highlight = function(group, highlight, palette) end,
+	-- })
 
-	require("nightfox").setup()
+	-- require("nightfox").setup()
 
-	require("gruvbox").setup({
-		terminal_colors = true, -- add neovim terminal colors
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "", -- can be "hard", "soft" or empty string
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = false,
-	})
+	-- require("gruvbox").setup({
+	-- 	terminal_colors = true, -- add neovim terminal colors
+	-- 	undercurl = true,
+	-- 	underline = true,
+	-- 	bold = true,
+	-- 	italic = {
+	-- 		strings = true,
+	-- 		emphasis = true,
+	-- 		comments = true,
+	-- 		operators = false,
+	-- 		folds = true,
+	-- 	},
+	-- 	strikethrough = true,
+	-- 	invert_selection = false,
+	-- 	invert_signs = false,
+	-- 	invert_tabline = false,
+	-- 	invert_intend_guides = false,
+	-- 	inverse = true, -- invert background for search, diffs, statuslines and errors
+	-- 	contrast = "", -- can be "hard", "soft" or empty string
+	-- 	palette_overrides = {},
+	-- 	overrides = {},
+	-- 	dim_inactive = false,
+	-- 	transparent_mode = false,
+	-- })
 
 	vim.cmd("colorscheme kanagawa")
 	vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg = "#43243B" }) -- kanagawa:winterRed
