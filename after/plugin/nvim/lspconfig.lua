30ab409b20f62a2fe384ec536e798ce0fd7a7256
Markowski, Robert <robert.markowski@dlr.de>
2025-01-20 13:43:39 +0100

    feat: lazy load LSPs

--- after/plugin/nvim/lspconfig.lua
+++ after/plugin/nvim/lspconfig.lua
@@ -11,6 +11,7 @@ if not vim.g.vscode then
 			"stylua",
 		},
 	})
+	local lspconfig = require("lspconfig")
 	local null_ls = require("null-ls")
 	null_ls.setup({
 		fallback_severity = vim.diagnostic.severity.HINT,
@@ -18,75 +19,77 @@ if not vim.g.vscode then
 			require("cspell").diagnostics.with({
 				config = { reload_on_cwd_change = true },
 			}),
-			null_ls.builtins.formatting.stylua,
 		},
 	})
-	local lspconfig = require("lspconfig")
-	lspconfig.basedpyright.setup({
-		settings = {
-			basedpyright = {
-				analysis = {
-					typeCheckingMode = "strict",
-					autoSearchPaths = true,
-					useLibraryCodeForTypes = true,
-					diagnosticMode = "workspace",
-					diagnosticSeverityOverrides = {
-						reportUnboundVariable = "error",
-						reportMissingModuleSource = "error",
+	require("mason-lspconfig").setup_handlers({
+		basedpyright = function()
+			lspconfig.basedpyright.setup({
+				settings = {
+					basedpyright = {
+						analysis = {
+							typeCheckingMode = "strict",
+							autoSearchPaths = true,
+							useLibraryCodeForTypes = true,
+							diagnosticMode = "workspace",
+							diagnosticSeverityOverrides = {
+								reportUnboundVariable = "error",
+								reportMissingModuleSource = "error",
+							},
+						},
 					},
 				},
-			},
-		},
-	})
-	-- lspconfig.pyright.setup({
-	--     settings = {
-	--         pyright = {
-	--             disableOrganizeImports = true,
-	--         },
-	--         python = {
-	--             analysis = {
-	--                 typeCheckingMode = "strict",
-	--                 autoSearchPaths = true,
-	--                 useLibraryCodeForTypes = true,
-	--                 diagnosticMode = "workspace",
-	--                 diagnosticSeverityOverrides = {
-	--                     reportUnboundVariable = "error",
-	--                     reportMissingModuleSource = "error",
-	--                 },
-	--             }
-	--         }
-	--     }
-	-- })
-	lspconfig.gopls.setup({})
-	lspconfig.jsonls.setup({
-		settings = {
-			json = {
-				validate = { enable = true },
-			},
-		},
-	})
-	lspconfig.lua_ls.setup({
-		on_init = function(client)
-			if client.workspace_folders then
-				local path = client.workspace_folders[1].name
-				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-					return
-				end
-			end
-
-			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-				runtime = { version = "LuaJIT" },
-				workspace = {
-					checkThirdParty = false,
-					library = { vim.env.VIMRUNTIME },
+			})
+			-- lspconfig.pyright.setup({
+			--     settings = {
+			--         pyright = {
+			--             disableOrganizeImports = true,
+			--         },
+			--         python = {
+			--             analysis = {
+			--                 typeCheckingMode = "strict",
+			--                 autoSearchPaths = true,
+			--                 useLibraryCodeForTypes = true,
+			--                 diagnosticMode = "workspace",
+			--                 diagnosticSeverityOverrides = {
+			--                     reportUnboundVariable = "error",
+			--                     reportMissingModuleSource = "error",
+			--                 },
+			--             }
+			--         }
+			--     }
+			-- })
+		end,
+		jsonls = function()
+			lspconfig.jsonls.setup({ settings = { json = { validate = { enable = true } } } })
+		end,
+		gopls = function()
+			lspconfig.gopls.setup({})
+		end,
+		lua_ls = function()
+			null_ls.register({ sources = { null_ls.builtins.formatting.stylua } })
+			lspconfig.lua_ls.setup({
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+							return
+						end
+					end
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = { version = "LuaJIT" },
+						workspace = {
+							checkThirdParty = false,
+							library = { vim.env.VIMRUNTIME },
+						},
+					})
+				end,
+				settings = {
+					Lua = {
+						format = { enable = false },
+					},
 				},
 			})
 		end,
-		settings = {
-			Lua = {
-				format = { enable = false },
-			},
-		},
 	})
 
 	vim.api.nvim_create_autocmd("LspAttach", {
