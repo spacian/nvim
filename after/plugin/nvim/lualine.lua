36d50f53d58b2aaacfe78babe412180c2e5a56cf
Robert Markowski <robert.markowski123@gmail.com>
2025-01-29 19:06:41 +0100

    feat: lualine cleanup

--- after/plugin/nvim/lualine.lua
+++ after/plugin/nvim/lualine.lua
@@ -1,27 +1,27 @@
 if not vim.g.vscode then
-	local function fixed_mode()
-		local mode_map = {
-			n = "NORMAL",
-			i = "INSERT",
-			v = "VISUAL",
-			V = "V-LINE",
-			[""] = "V-BLOCK",
-			c = "COMMAND",
-			R = "REPLACE",
-			t = "TERMINAL",
-		}
-		local mode = vim.api.nvim_get_mode().mode
-		local mode_name = mode_map[mode] or "NORMAL"
-		return string.format("%-8s", mode_name)
-	end
+	-- local function fixed_mode()
+	-- 	local mode_map = {
+	-- 		n = "NORMAL",
+	-- 		i = "INSERT",
+	-- 		v = "VISUAL",
+	-- 		V = "V-LINE",
+	-- 		[""] = "V-BLOCK",
+	-- 		c = "COMMAND",
+	-- 		R = "REPLACE",
+	-- 		t = "TERMINAL",
+	-- 	}
+	-- 	local mode = vim.api.nvim_get_mode().mode
+	-- 	local mode_name = mode_map[mode] or "NORMAL"
+	-- 	return string.format("%-8s", mode_name)
+	-- end
 
-	local function git()
-		local gitsigns = vim.b.gitsigns_status_dict
-		if gitsigns and (gitsigns.added ~= 0 or gitsigns.changed ~= 0 or gitsigns.removed ~= 0) then
-			return "✦"
-		end
-		return " "
-	end
+	-- local function git()
+	-- 	local gitsigns = vim.b.gitsigns_status_dict
+	-- 	if gitsigns and (gitsigns.added ~= 0 or gitsigns.changed ~= 0 or gitsigns.removed ~= 0) then
+	-- 		return "✦"
+	-- 	end
+	-- 	return " "
+	-- end
 
 	local indicator_symbol = "●"
 	local function diagnostic(level)
@@ -49,12 +49,12 @@ if not vim.g.vscode then
 		return diagnostic(vim.diagnostic.severity.HINT)
 	end
 
-	local function project()
-		local dir = vim.fn.getcwd()
-		dir = dir:gsub("\\", "/")
-		dir = dir:match(".*/(.+)$") or ""
-		return dir
-	end
+	-- local function project()
+	-- 	local dir = vim.fn.getcwd()
+	-- 	dir = dir:gsub("\\", "/")
+	-- 	dir = dir:match(".*/(.+)$") or ""
+	-- 	return dir
+	-- end
 
 	local theme = require("lualine.themes.auto")
 	theme.insert.c = theme.normal.c
@@ -72,7 +72,7 @@ if not vim.g.vscode then
 			always_divide_middle = false,
 		},
 		sections = {
-			lualine_a = { fixed_mode },
+			lualine_a = {},
 			lualine_b = {},
 			lualine_c = {
 				{ error_ind, color = { fg = "#FF0000" } },
@@ -81,10 +81,7 @@ if not vim.g.vscode then
 				{ note_ind, color = { fg = "#005599" } },
 			},
 			lualine_x = { { "filename", path = 4, file_status = false } },
-			lualine_y = {
-				{ git, color = { fg = theme.normal.c.fg } },
-				{ project, color = { fg = theme.normal.c.fg } },
-			},
+			lualine_y = {},
 			lualine_z = {},
 		},
 	})
