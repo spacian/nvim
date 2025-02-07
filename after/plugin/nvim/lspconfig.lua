163be32e143681ca1c5fe5fc5ec080ef04c5aae3
Robert Markowski <robert.markowski123@gmail.com>
2025-02-07 09:38:52 +0100

    feat: add command to force reload cspell

--- after/plugin/nvim/lspconfig.lua
+++ after/plugin/nvim/lspconfig.lua
@@ -16,6 +16,15 @@ if not vim.g.vscode then
 	local lspconfig = require("lspconfig")
 	local null_ls = require("null-ls")
 	local cspell_last_session = ""
+	local force_cspell_reload = false
+	vim.api.nvim_create_user_command("CSpellReload", function()
+		if BufIsSpecial() then
+			print("cannot reload from special buffer")
+			return
+		end
+		force_cspell_reload = true
+		vim.cmd("noa w|e")
+	end, {})
 	null_ls.setup({
 		fallback_severity = vim.diagnostic.severity.HINT,
 		sources = {
@@ -28,8 +37,13 @@ if not vim.g.vscode then
 						return vim.fn.getcwd()
 					end,
 					reset_cspell = function(params)
-						if params ~= nil and params.cwd ~= nil and vim.v.this_session ~= cspell_last_session then
+						if
+							params ~= nil
+							and params.cwd ~= nil
+							and (vim.v.this_session ~= cspell_last_session or force_cspell_reload)
+						then
 							cspell_last_session = vim.v.this_session
+							force_cspell_reload = false
 							return true
 						else
 							return false
