9e3240bf4002f73decb581b564655fb1723e9877
Robert Markowski <robert.markowski123@gmail.com>
2025-02-10 08:10:33 +0100

    feat: only install necessary tree-sitter syntax

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,8 +1,25 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
+	ensure_installed = {
+		"bash",
+		"csv",
+		"go",
+		"json",
+		"jsonc",
+		"lua",
+		"luadoc",
+		"luap",
+		"markdown",
+		"powershell",
+		"python",
+		"toml",
+		"regex",
+		"vimdoc",
+		"xml",
+		"yaml",
+	},
 	highlight = {
 		enable = not vim.g.vscode,
-		additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = true,
 		disable = { "latex" },
 	},
 	incremental_selection = {
