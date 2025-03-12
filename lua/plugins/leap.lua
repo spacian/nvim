74e5c7a7d9bbc209a3aa273a877b9d51be285ad9
Robert Markowski <robert.markowski123@gmail.com>
2025-03-12 17:03:43 +0100

    feat: improve leap labels

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,14 +3,17 @@ return {
 	enabled = true,
 	lazy = false,
 	config = function()
-		require("leap").opts.equivalence_classes = {
-			" \t\r\n",
-			"'\"`",
-			"aä",
-			"oö",
-			"uü",
-			"sß",
-		}
+		require("leap").setup({
+			safe_labels = "fmtunqzFMTUNQZ",
+			equivalence_classes = {
+				" \t\r\n",
+				"'\"`",
+				"aä",
+				"oö",
+				"uü",
+				"sß",
+			},
+		})
 		vim.keymap.set({ "n" }, "gj", "<Plug>(leap-forward)")
 		vim.keymap.set({ "n" }, "gk", "<Plug>(leap-backward)")
 	end,
