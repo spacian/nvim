ece72766e20b6d3008f03d49abc1ba0a19a4242a
Robert Markowski <robert.markowski123@gmail.com>
2025-03-17 16:13:21 +0100

    feat: nvim spider - only match words

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,10 +1,12 @@
 return {
 	{
 		"chrisgrieser/nvim-spider",
-        enabled = true,
-        lazy = false,
+		enabled = true,
+		lazy = false,
 		config = function()
-			require("spider").setup({ skipInsignificantPunctuation = true, subwordMovement = false })
+			require("spider").setup({
+				customPatterns = { patterns = { "[%w_]+" }, overrideDefault = true },
+			})
 			vim.keymap.set({ "n", "v" }, "w", "<cmd>lua require('spider').motion('w')<enter>")
 			vim.keymap.set({ "n", "v" }, "e", "<cmd>lua require('spider').motion('e')<enter>")
 			vim.keymap.set({ "n", "v" }, "b", "<cmd>lua require('spider').motion('b')<enter>")
