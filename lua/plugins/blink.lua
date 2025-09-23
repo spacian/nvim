b8f4e04a52a6f3163b8360bf46bcd4607e5001bd
Robert Markowski <robert.markowski123@gmail.com>
2025-09-23 13:54:47 +0200

    feat: use transform instead of custom comparators

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,10 +1,82 @@
-local starts_with_underscore = function(label, n)
-	return label:sub(1, n) == string.sub("__", 1, n)
+local higher_equals = function(a, b)
+	local ends_with_equals = function(label)
+		return label:sub(#label, #label) == "="
+	end
+	local a_ = ends_with_equals(a.label or "")
+	local b_ = ends_with_equals(b.label or "")
+	if a_ ~= b_ then
+		return a_
+	end
+	return nil
 end
 
-local ends_with_equals = function(label)
-	return label:sub(#label, #label) == "="
+local lower_underscores = function(a, b)
+	local starts_with_underscore = function(label, n)
+		return label:sub(1, n) == string.sub("__", 1, n)
+	end
+	local a_ = starts_with_underscore(a.label or "", 2)
+	local b_ = starts_with_underscore(b.label or "", 2)
+	if a_ ~= b_ then
+		return not a_
+	end
+	return nil
 end
+
+local function lower_basemodel(a, b)
+	local function is_basemodel_method(label)
+		return label:match("^__") or label:match("^model_")
+	end
+	local a_ = is_basemodel_method(a.label)
+	local b_ = is_basemodel_method(b.label)
+	if a_ ~= b_ then
+		return not a_
+	end
+end
+
+local function lower_deprecated(a, b)
+	-- if a.source_id == "lsp" then
+	-- 	print(vim.inspect(a))
+	-- end
+	local function is_deprecated(item)
+		return item.completion_item and item.completion_item.deprecated or false
+	end
+	local a_ = is_deprecated(a)
+	local b_ = is_deprecated(b)
+	if a_ ~= b_ then
+		return not a_
+	end
+end
+
+local transform = function(_, items)
+	local function is_basemodel_method(label)
+		return label:match("^model_")
+	end
+	local starts_with_underscore = function(label, n)
+		return label:sub(1, n) == string.sub("__", 1, n)
+	end
+	local ends_with_equals = function(label)
+		return label:sub(#label, #label) == "="
+	end
+	for _, item in ipairs(items) do
+		if item.deprecated or (item.tags and vim.tbl_contains(item.tags, 1)) then
+			item.score_offset = item.score_offset - 5
+		end
+		if is_basemodel_method(item.label) then
+			item.score_offset = item.score_offset - 1
+		end
+		if starts_with_underscore(item.label, 1) then
+			item.score_offset = item.score_offset - 1
+		end
+		if starts_with_underscore(item.label, 2) then
+			item.score_offset = item.score_offset - 1
+		end
+		if ends_with_equals(item.label) then
+			item.score_offset = item.score_offset + 5
+		end
+	end
+	return items
+end
+
 return {
 	{
 		"saghen/blink.cmp",
@@ -27,25 +99,14 @@ return {
 			},
 			sources = {
 				default = { "lsp", "path", "buffer" },
+				transform_items = transform,
 			},
 			fuzzy = {
 				sorts = {
-					function(a, b)
-						local a_ = ends_with_equals(a.label or "")
-						local b_ = ends_with_equals(b.label or "")
-						if a_ ~= b_ then
-							return a_
-						end
-						return nil
-					end,
-					function(a, b)
-						local a_ = starts_with_underscore(a.label or "", 2)
-						local b_ = starts_with_underscore(b.label or "", 2)
-						if a_ ~= b_ then
-							return not a_
-						end
-						return nil
-					end,
+					-- higher_equals,
+					-- lower_deprecated,
+					-- lower_underscores,
+					-- lower_basemodel,
 					"exact",
 					"score",
 					"sort_text",
