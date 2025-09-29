9f878a7a01c7e583fe08ae02fccebbd822c8ac13
Robert Markowski <robert.markowski123@gmail.com>
2025-09-29 10:09:23 +0200

    feat: direction specific configuration

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -4,20 +4,27 @@ return {
 		enabled = true,
 		lazy = false,
 		config = function()
-			require("spider").setup({
-				customPatterns = {
-					patterns = {
-						"[%wäöüÄÖÜß_]+",
-						"%S$",
-						"^%S",
-					},
-					overrideDefault = true,
-				},
-			})
-			vim.keymap.set({ "n", "v" }, "w", "<cmd>lua require('spider').motion('w')<enter>")
-			vim.keymap.set({ "n", "v" }, "e", "<cmd>lua require('spider').motion('e')<enter>")
-			vim.keymap.set({ "n", "v" }, "b", "<cmd>lua require('spider').motion('b')<enter>")
-			vim.keymap.set({ "n", "v" }, "ge", "<cmd>lua require('spider').motion('ge')<enter>")
+			require("spider").setup()
+			local opening = {
+				customPatterns = { "[%wäöüÄÖÜß_]+", "[%{%[%(]+" },
+				overrideDefault = true,
+			}
+			local closing = {
+				customPatterns = { "[%wäöüÄÖÜß_]+", "[%}%]%)]+" },
+				overrideDefault = true,
+			}
+			vim.keymap.set({ "n", "v" }, "w", function()
+				require("spider").motion("w", opening)
+			end)
+			vim.keymap.set({ "n", "v" }, "e", function()
+				require("spider").motion("e", closing)
+			end)
+			vim.keymap.set({ "n", "v" }, "b", function()
+				require("spider").motion("b", opening)
+			end)
+			vim.keymap.set({ "n", "v" }, "ge", function()
+				require("spider").motion("ge", closing)
+			end)
 		end,
 	},
 }
