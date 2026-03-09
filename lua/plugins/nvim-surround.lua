3863c5a52df87ea3b8397818969980b303e8a845
Robert Markowski <robert.markowski123@gmail.com>
2026-03-09 13:48:30 +0100

    fix(plugins): fix nvim surround keymap

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,10 +3,9 @@ return {
     "kylechui/nvim-surround",
     enabled = true,
     lazy = false,
-    opts = {
-      keymaps = {
-        visual = "S",
-      },
-    },
+    config = function()
+      require("nvim-surround").setup()
+      vim.keymap.set("v", "S", "<Plug>(nvim-surround-visual)")
+    end,
   },
 }
