a3234a0811b4179aefb4eaa0a3dd88b81a94bdcf
Robert Markowski <robert.markowski123@gmail.com>
2026-05-12 17:07:53 +0200

    fix(lsp): fix jsonls

--- lua/lsp/server/jsonls.lua
+++ lua/lsp/server/jsonls.lua
@@ -1,8 +1,8 @@
-vim.lsp.config("json-lsp", {
+vim.lsp.config("jsonls", {
   on_init = function(client)
     client.server_capabilities.semanticTokensProvider = false
   end,
   settings = { json = { validate = { enable = true } } },
   capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
-vim.lsp.enable("json-lsp")
+vim.lsp.enable("jsonls")
