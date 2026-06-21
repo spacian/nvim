a138447ce1e5bbfeeb7e0696adefe550c0ce9526
Robert Markowski <robert.markowski123@gmail.com>
2026-06-21 07:23:03 +0200

    feat(navigation): make prev/next hunk repeatable

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,45 +4,62 @@ return {
     enabled = not vim.g.vscode,
     lazy = false,
     config = function()
-      local sign = "▕"
-      local signs = {
-        add = { text = sign },
-        change = { text = sign },
-        changedelete = { text = sign },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        untracked = { text = sign },
-      }
-      require("gitsigns").setup({
-        sign_priority = 10000,
-        signs = signs,
-        signs_staged = signs,
-        signs_staged_enable = true,
-        attach_to_untracked = true,
-        signcolumn = true,
-        watch_gitdir = {
-          interval = 1000,
-          follow_files = true,
-        },
-        on_attach = function(bufnr)
-          local gitsigns = require("gitsigns")
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TreesitterTextobjectsSetupDone",
+        callback = function()
+          local sign = "▕"
+          local signs = {
+            add = { text = sign },
+            change = { text = sign },
+            changedelete = { text = sign },
+            delete = { text = "_" },
+            topdelete = { text = "‾" },
+            untracked = { text = sign },
+          }
+          require("gitsigns").setup({
+            sign_priority = 10000,
+            signs = signs,
+            signs_staged = signs,
+            signs_staged_enable = true,
+            attach_to_untracked = true,
+            signcolumn = true,
+            watch_gitdir = {
+              interval = 1000,
+              follow_files = true,
+            },
+            on_attach = function(bufnr)
+              local gitsigns = require("gitsigns")
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
 
-          map("n", "]g", function()
-            gitsigns.nav_hunk("next")
-          end)
+              local jump_hunk = function(opts)
+                if opts.forward then
+                  gitsigns.nav_hunk("next")
+                else
+                  gitsigns.nav_hunk("prev")
+                end
+              end
 
-          map("n", "[g", function()
-            gitsigns.nav_hunk("prev")
-          end)
-          map("n", "<leader>gt", gitsigns.toggle_deleted)
-          map({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>")
-          map({ "o", "x" }, "ag", ":<C-U>Gitsigns select_hunk<CR>")
+              local ts_repeat_move =
+                require("nvim-treesitter-textobjects.repeatable_move")
+              local repeatable_jump_hunk =
+                ts_repeat_move.make_repeatable_move(jump_hunk)
+
+              map("n", "]g", function()
+                repeatable_jump_hunk({ forward = true })
+              end)
+              map("n", "[g", function()
+                repeatable_jump_hunk({ forward = false })
+              end)
+              map("n", "<leader>gt", gitsigns.toggle_deleted)
+              map({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>")
+              map({ "o", "x" }, "ag", ":<C-U>Gitsigns select_hunk<CR>")
+            end,
+          })
         end,
       })
     end,
--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -4,6 +4,7 @@ return {
     enabled = true,
     branch = "main",
     lazy = false,
+    after = "gitsigns",
     init = function()
       vim.g.no_plugin_maps = true
     end,
@@ -25,6 +26,10 @@ return {
               include_surrounding_whitespace = false,
             },
           })
+          vim.api.nvim_exec_autocmds(
+            "User",
+            { pattern = "TreesitterTextobjectsSetupDone" }
+          )
           local keymap_select = function(lhs, obj)
             vim.keymap.set({ "x", "o" }, lhs, function()
               require("nvim-treesitter-textobjects.select").select_textobject(
@@ -148,40 +153,6 @@ return {
             ts_repeat_move.builtin_T_expr,
             { expr = true }
           )
-          local hint = vim.diagnostic.severity.HINT
-          local info = vim.diagnostic.severity.INFO
-          local warn = vim.diagnostic.severity.WARN
-          local error = vim.diagnostic.severity.ERROR
-
-          local get_severity = function()
-            local count = vim.diagnostic.count(0)
-            for _, s in ipairs({ error, warn, info, hint }) do
-              if count[s] ~= nil and count[s] > 0 then
-                return s
-              end
-            end
-            return nil
-          end
-
-          local jump_diagnostic = function(opts)
-            if opts.forward then
-              vim.diagnostic.jump({ count = vim.v.count1, severity = get_severity() })
-            else
-              vim.diagnostic.jump({ count = -vim.v.count1, severity = get_severity() })
-            end
-          end
-
-          local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
-          local repeatable_jump_diagnostic =
-            ts_repeat_move.make_repeatable_move(jump_diagnostic)
-
-          vim.keymap.set("n", "]d", function()
-            repeatable_jump_diagnostic({ forward = true })
-          end)
-
-          vim.keymap.set("n", "[d", function()
-            repeatable_jump_diagnostic({ forward = false })
-          end)
         end,
       })
     end,
