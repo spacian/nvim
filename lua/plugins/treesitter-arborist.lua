3fd98a4dd3171484a8f06e057dfccdf374e6d053
Robert Markowski <robert.markowski123@gmail.com>
2026-07-03 08:55:22 +0200

    feat(treesitter): add incremental selection

--- lua/plugins/treesitter-arborist.lua
+++ lua/plugins/treesitter-arborist.lua
@@ -37,6 +37,33 @@ return {
             vim.treesitter.start(args.buf, "markdown")
           end,
         })
+
+        vim.keymap.set(
+          { "x" },
+          "i",
+          function()
+            if vim.treesitter.get_parser(nil, nil, { error = false }) then
+              require("vim.treesitter._select").select_parent(vim.v.count1)
+            else
+              vim.lsp.buf.selection_range(vim.v.count1)
+            end
+          end,
+          { desc = "Select parent treesitter node or outer incremental lsp selections" }
+        )
+
+        vim.keymap.set(
+          { "x" },
+          "v",
+          function()
+            if vim.treesitter.get_parser(nil, nil, { error = false }) then
+              require("vim.treesitter._select").select_child(vim.v.count1)
+            else
+              vim.lsp.buf.selection_range(-vim.v.count1)
+            end
+          end,
+          { desc = "Select child treesitter node or inner incremental lsp selections" }
+        )
+
         vim.api.nvim_exec_autocmds("User", { pattern = "TreesitterSetupDone" })
       end,
     })
