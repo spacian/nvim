58edc358f66184c3aae2e8346d5fe0b21c308885
Robert Markowski <robert.markowski123@gmail.com>
2026-07-06 17:17:43 +0200

    feat(cmd): reduce cmd clear complexity

--- lua/autocmd/clear_cmd.lua
+++ lua/autocmd/clear_cmd.lua
@@ -1,21 +1,5 @@
-local timer = vim.uv.new_timer()
-
-if timer ~= nil then
-  local function schedule_clear()
-    timer:stop()
-    timer:start(
-      3000,
-      0,
-      vim.schedule_wrap(function()
-        vim.api.nvim_echo({ { "" } }, false, {})
-      end)
-    )
-  end
-
-  vim.api.nvim_create_autocmd(
-    { "BufEnter", "InsertEnter", "TextChanged", "CmdlineLeave" },
-    {
-      callback = schedule_clear,
-    }
-  )
-end
+vim.api.nvim_create_autocmd({ "BufEnter", "InsertEnter" }, {
+  callback = function()
+    vim.api.nvim_echo({ { "" } }, false, {})
+  end,
+})
