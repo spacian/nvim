7a06bdad680e0210c9a9b1e00adc29d0a8298883
Robert Markowski <robert.markowski123@gmail.com>
2026-07-07 10:24:14 +0200

    feat(plugins): remove select-undo

--- lua/plugins/undo.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "SunnyTamang/select-undo.nvim",
-  config = function()
-    require("select-undo").setup({ mapping = false })
-    vim.keymap.set("x", "u", ":'<,'>SelectUndoPartial<enter>gv")
-  end,
-}
