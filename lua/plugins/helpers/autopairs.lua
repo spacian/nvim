5a742c461fbd2e87b1c815056c9dda4c1a3c0a36
Robert Markowski <robert.markowski123@gmail.com>
2026-07-17 09:32:30 +0200

    feat(plugins): autopairs - disable in strings

--- lua/plugins/helpers/autopairs.lua
+++ lua/plugins/helpers/autopairs.lua
@@ -2,8 +2,22 @@ return {
   "windwp/nvim-autopairs",
   config = function()
     local autopairs = require("nvim-autopairs")
+    local strings = {
+      "string",
+      "string_literal",
+      "template_string",
+      "raw_string_literal",
+    }
     autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = strings,
+        python = strings,
+      },
       ignored_next_char = [=[[^%)^%}^%]]]=],
+      disable_in_visualblock = true,
+      enable_bracket_in_quote = false,
+      enable_afterquote = false,
     })
     autopairs.remove_rule("'")
     autopairs.remove_rule("`")
