ef8d480845831a0db983ef0812081829f9ed31de
Robert Markowski <robert.markowski123@gmail.com>
2026-07-27 14:49:58 +0200

    feat(terminal): add picker for terminals

--- lua/keymaps/nvim/terminal.lua
+++ lua/keymaps/nvim/terminal.lua
@@ -27,20 +27,20 @@ function M.open(name)
   local buf = terms[project][name]
 
   if buf and vim.api.nvim_buf_is_valid(buf) then
-    vim.cmd("silent buffer " .. buf)
-    vim.cmd("silent startinsert")
+    vim.cmd.buffer(buf)
+    vim.cmd.startinsert()
     return
   end
 
   if vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.cmd("silent terminal pwsh")
+    vim.cmd.terminal("pwsh")
   else
-    vim.cmd("silent terminal")
+    vim.cmd.terminal()
   end
   buf = vim.api.nvim_get_current_buf()
   terms[project][name] = buf
 
-  vim.cmd("silent startinsert")
+  vim.cmd.startinsert()
 end
 
 vim.keymap.set("n", "<leader>ot", function()
@@ -48,8 +48,51 @@ vim.keymap.set("n", "<leader>ot", function()
 end)
 
 vim.keymap.set("n", "<leader>oT", function()
-  local name = vim.fn.input("terminal name: ")
-  if name and #name > 0 then
-    M.open(name)
+  local get_items = function()
+    local items = {}
+    for name, bufnr in pairs(terms[vim.fn.getcwd()] or {}) do
+      if name ~= "" and vim.api.nvim_buf_is_valid(bufnr) then
+        items[#items + 1] = {
+          text = name,
+          label = name,
+          buf = bufnr,
+        }
+      end
+    end
+    items[#items + 1] = { text = "<new>", label = "<new>", new = true }
+    return items
   end
+  require("snacks").picker({
+    title = "Terminals",
+    layout = "select",
+    items = get_items(),
+    confirm = function(picker, item)
+      picker:close()
+      if item.new then
+        item.label = vim.fn.input("terminal name: ")
+      end
+      if item.label and item.label ~= "" then
+        vim.schedule(function()
+          M.open(item.label)
+        end)
+      end
+    end,
+    actions = {
+      delete = function(picker, item)
+        if item and item.label ~= "new" then
+          vim.cmd("silent bd! " .. terms[vim.fn.getcwd()][item.label])
+          terms[vim.fn.getcwd()][item.label] = nil
+          picker.opts.items = get_items()
+          picker:refresh()
+        end
+      end,
+    },
+    win = {
+      input = {
+        keys = {
+          ["<c-x>"] = { "delete", mode = { "i", "n" } },
+        },
+      },
+    },
+  })
 end)
