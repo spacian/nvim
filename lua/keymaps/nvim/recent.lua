eb4cf7b5b17793ca314205a5e29758da3f7290a6
Robert Markowski <robert.markowski123@gmail.com>
2026-08-01 11:31:17 +0200

    feat(plugins): grapple - move previous logic to custom implementation

--- lua/keymaps/nvim/recent.lua
+++ lua/keymaps/nvim/recent.lua
@@ -14,25 +14,26 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 local jumplist = require("keymaps.nvim.jumplist")
 
+local get_items = function()
+  local items = {}
+  for bufname, nr in pairs(files[vim.fn.getcwd()] or {}) do
+    local bufnr = vim.fn.bufnr(bufname)
+    if bufnr ~= -1 and not BufIsSpecial(bufnr) then
+      items[#items + 1] = {
+        text = bufname,
+        file = bufname,
+        nr = nr,
+      }
+    end
+  end
+  table.sort(items, function(a, b)
+    return a.nr > b.nr
+  end)
+  return items
+end
+
 vim.keymap.set("n", "<leader>or", function()
   jumplist.register()
-  local get_items = function()
-    local items = {}
-    for bufname, nr in pairs(files[vim.fn.getcwd()] or {}) do
-      local bufnr = vim.fn.bufnr(bufname)
-      if bufnr ~= -1 and not BufIsSpecial(bufnr) then
-        items[#items + 1] = {
-          text = bufname,
-          file = bufname,
-          nr = nr,
-        }
-      end
-    end
-    table.sort(items, function(a, b)
-      return a.nr > b.nr
-    end)
-    return items
-  end
   require("snacks").picker({
     title = "Recent",
     layout = "select",
@@ -55,3 +56,51 @@ vim.keymap.set("n", "<leader>or", function()
     },
   })
 end)
+
+vim.keymap.set("n", "<leader>h", function()
+  jumplist.register()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  for _, item in ipairs(get_items()) do
+    if item.file ~= bufname then
+      vim.cmd.edit(item.file)
+      break
+    end
+  end
+end)
+
+local function is_lazygit_buffer()
+  return vim.api.nvim_buf_get_name(0):match("lazygit") == "lazygit"
+end
+
+local enter = vim.api.nvim_replace_termcodes("<enter>", true, true, true)
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    if is_lazygit_buffer() then
+      return
+    end
+    vim.opt_local.statuscolumn = ""
+    vim.keymap.set({ "n", "v" }, "<c-u>", "", { buffer = true, silent = true })
+    vim.keymap.set({ "n", "v" }, "<c-d>", "", { buffer = true, silent = true })
+    vim.keymap.set({ "t", "n", "v" }, "<c-u><c-y>", function()
+      vim.fn.feedkeys('cd "' .. vim.fn.getcwd() .. '"' .. enter)
+    end, { buffer = true })
+    vim.keymap.set({ "t", "n", "v" }, "<c-u><c-o>", function()
+      local folder = get_items()[1].file:match("(.*)\\.*")
+      if folder ~= nil then
+        vim.fn.feedkeys('cd "' .. folder .. '"' .. enter)
+      end
+    end, { buffer = true })
+    vim.keymap.set({ "t", "n", "v" }, "<c-u><c-i>", function()
+      vim.cmd("silent bd!")
+    end, { buffer = true })
+    vim.keymap.set({ "t", "n", "v" }, "<c-u><c-u>", function()
+      if vim.fn.winnr("$") > 1 then
+        vim.cmd("silent close")
+        return
+      else
+        vim.cmd.edit(get_items()[1].file)
+      end
+    end, { buffer = true })
+  end,
+})
--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -5,44 +5,8 @@ return {
     config = function()
       local jumplist = require("keymaps.nvim.jumplist")
       local grapple = require("grapple")
-      local enter = vim.api.nvim_replace_termcodes("<enter>", true, true, true)
 
-      local PREV1 = function(path)
-        return { name = "prev1", scope = "prev", path = path }
-      end
-
-      local PREV2 = function(path)
-        return { name = "prev2", scope = "prev", path = path }
-      end
-
-      grapple.setup({
-        scope = "prev",
-        scopes = {
-          {
-            name = "prev",
-            resolver = function()
-              return "prev", nil
-            end,
-          },
-        },
-      })
       grapple.setup({ scope = "cwd" })
-
-      vim.keymap.set("n", "<leader>h", function()
-        jumplist.register()
-        if grapple.exists(PREV1()) then
-          if not BufIsSpecial() then
-            vim.cmd("silent noa w")
-          end
-          local path = grapple.find(PREV1()).path
-          local bufname = vim.api.nvim_buf_get_name(0)
-          if path == bufname and grapple.exists(PREV2()) then
-            grapple.select(PREV2())
-          else
-            grapple.select(PREV1())
-          end
-        end
-      end)
 
       vim.keymap.set("n", "<leader>ob", function()
         jumplist.register()
@@ -86,80 +50,12 @@ return {
         end
       end)
 
-      local last_bufname = ""
-      vim.api.nvim_create_autocmd({ "BufEnter" }, {
-        callback = function()
-          vim.defer_fn(function()
-            local is_float = vim.api.nvim_win_get_config(0).relative ~= ""
-            if is_float then
-              return
-            end
-            local bufname = vim.api.nvim_buf_get_name(0)
-            if last_bufname ~= "" and bufname ~= last_bufname then
-              if grapple.exists(PREV1()) then
-                local prev_path = grapple.find(PREV1()).path
-                if last_bufname ~= prev_path then
-                  grapple.tag(PREV2(prev_path))
-                end
-              end
-              grapple.tag(PREV1(last_bufname))
-            end
-            if not BufIsSpecial() then
-              last_bufname = bufname
-              vim.o.cursorline = true
-            else
-              last_bufname = ""
-            end
-          end, 100)
-        end,
-      })
-
-      local is_lazygit_buffer = function()
-        return vim.api.nvim_buf_get_name(0):match("lazygit") == "lazygit"
-      end
-
-      vim.api.nvim_create_autocmd("TermOpen", {
-        pattern = "*",
-        callback = function()
-          if is_lazygit_buffer() then
-            return
-          end
-          vim.opt_local.statuscolumn = ""
-          vim.keymap.set({ "n", "v" }, "<c-u>", "", { buffer = true, silent = true })
-          vim.keymap.set({ "n", "v" }, "<c-d>", "", { buffer = true, silent = true })
-          vim.keymap.set({ "t", "n", "v" }, "<c-u><c-y>", function()
-            vim.fn.feedkeys('cd "' .. vim.fn.getcwd() .. '"' .. enter)
-          end, { buffer = true })
-          vim.keymap.set({ "t", "n", "v" }, "<c-u><c-o>", function()
-            if not grapple.exists(PREV1()) then
-              return
-            end
-            local folder = grapple.find(PREV1()).path:match("(.*)\\.*")
-            if folder ~= nil then
-              vim.fn.feedkeys('cd "' .. folder .. '"' .. enter)
-            end
-          end, { buffer = true })
-          vim.keymap.set({ "t", "n", "v" }, "<c-u><c-i>", function()
-            vim.cmd("silent bd!")
-          end, { buffer = true })
-          vim.keymap.set({ "t", "n", "v" }, "<c-u><c-u>", function()
-            if vim.fn.winnr("$") > 1 then
-              vim.cmd("silent close")
-              return
-            elseif grapple.exists(PREV1()) then
-              grapple.select(PREV1())
-            end
-          end, { buffer = true })
-        end,
-      })
-
       vim.api.nvim_create_autocmd("SessionLoadPost", {
         callback = function(_)
           vim.defer_fn(function()
             jumplist.reset()
             jumplist.register()
             vim.cmd("clearjumps")
-            vim.cmd("silent Grapple reset scope=prev")
           end, 250)
         end,
       })
