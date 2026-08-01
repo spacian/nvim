e1c343533c5fb9b45a095f40f0ed03df73e6b30a
Robert Markowski <robert.markowski123@gmail.com>
2026-08-01 11:39:59 +0200

    feat(panels): simplify panel creation

--- lua/keymaps/nvim/panels.lua
+++ lua/keymaps/nvim/panels.lua
@@ -19,91 +19,35 @@ vim.keymap.set({ "", "l", "t" }, "<c-right>", "<c-w><")
 vim.keymap.set({ "t" }, "<c-n>", [[<c-\><c-n>]])
 
 vim.keymap.set({ "", "l", "t" }, "<a-h>", function()
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>h]], true, false, true),
-    "n",
-    true
-  )
+  Feedkeys([[<c-\><c-n><c-w>h]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<a-j>", function()
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>j]], true, false, true),
-    "n",
-    true
-  )
+  Feedkeys([[<c-\><c-n><c-w>j]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<a-k>", function()
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>k]], true, false, true),
-    "n",
-    true
-  )
+  Feedkeys([[<c-\><c-n><c-w>k]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<a-l>", function()
-  vim.api.nvim_feedkeys(
-    vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>l]], true, false, true),
-    "n",
-    true
-  )
+  Feedkeys([[<c-\><c-n><c-w>l]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<c-a-h>", function()
-  if vim.fn.winnr("$") == 1 then
-    vim.cmd("vsplit | b#")
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>h]], true, false, true),
-      "n",
-      true
-    )
-  else
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>H]], true, false, true),
-      "n",
-      true
-    )
-  end
+  vim.cmd("vsplit | b#")
+  Feedkeys([[<c-\><c-n><c-w>h]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<c-a-j>", function()
-  if vim.fn.winnr("$") == 1 then
-    vim.cmd("split | b#")
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>j]], true, false, true),
-      "n",
-      true
-    )
-  else
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>J]], true, false, true),
-      "n",
-      true
-    )
-  end
+  vim.cmd("split | b#")
+  Feedkeys([[<c-\><c-n><c-w>j]])
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<c-a-k>", function()
-  if vim.fn.winnr("$") == 1 then
-    vim.cmd("split | b# | wincmd x")
-  else
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>K]], true, false, true),
-      "n",
-      true
-    )
-  end
+  vim.cmd("split | b# | wincmd x")
 end)
 
 vim.keymap.set({ "", "l", "t" }, "<c-a-l>", function()
-  if vim.fn.winnr("$") == 1 then
-    vim.cmd("vsplit | b# | wincmd x")
-  else
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes([[<c-\><c-n><c-w>L]], true, false, true),
-      "n",
-      true
-    )
-  end
+  vim.cmd("vsplit | b# | wincmd x")
 end)
