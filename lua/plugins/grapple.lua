870fc1ff129837aa340ee0d3611d6680db9b1505
Robert Markowski <robert.markowski123@gmail.com>
2026-08-04 11:11:49 +0200

    feat(plugins): grapple - auto resize window

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -48,6 +48,31 @@ return {
           grapple.select({ index = index })
         end
       end)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "Grapple",
+        callback = function()
+          vim.schedule(function()
+            local win = vim.api.nvim_get_current_win()
+            local config = vim.api.nvim_win_get_config(win)
+            if config.relative == "" then
+              return
+            end
+
+            local buf = vim.api.nvim_win_get_buf(win)
+            local max_width = 78
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+              max_width = math.max(max_width, vim.fn.strdisplaywidth(line))
+            end
+
+            config.width = math.min(max_width + 2, vim.o.columns - 8)
+            config.col = math.floor((vim.o.columns - config.width) / 2)
+            config.border = "rounded"
+
+            vim.api.nvim_win_set_config(win, config)
+          end)
+        end,
+      })
     end,
   },
 }
