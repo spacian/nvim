15aae422b9ac32007af01352ffbb01a6246dfacc
Robert Markowski <robert.markowski123@gmail.com>
2026-08-06 16:07:07 +0200

    feat(plugins): textobjects - do not invert direction on invert search

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -127,11 +127,11 @@ return {
           keymap_move_previous_end("[K", "@comment.outer")
 
           local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
-          vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+          vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
           vim.keymap.set(
             { "n", "x", "o" },
             ",",
-            ts_repeat_move.repeat_last_move_previous
+            ts_repeat_move.repeat_last_move_opposite
           )
           vim.keymap.set(
             { "n", "x", "o" },
