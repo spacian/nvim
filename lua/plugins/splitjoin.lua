9a0a2ac2068d74a68447f9ed3a5ad13bb8fa3ed8
Robert Markowski <robert.markowski123@gmail.com>
2026-08-06 08:50:48 +0200

    feat(plugins): splitjoin - remove max join length

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     local sj = require("treesj")
-    sj.setup({ max_join_length = 88, use_default_keymaps = false })
+    sj.setup({ use_default_keymaps = false })
     vim.keymap.set("n", "<leader>l", sj.toggle)
   end,
 }
