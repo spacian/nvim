8d58fd4c81996e52df695e020f2b5894cd968e17
Robert Markowski <robert.markowski123@gmail.com>
2026-08-10 15:57:32 +0200

    feat(terminal): clear input before custom commands

--- lua/keymaps/terminal.lua
+++ lua/keymaps/terminal.lua
@@ -2,18 +2,18 @@ if vim.loop.os_uname().sysname == "Windows_NT" then
   vim.keymap.set(
     { "t" },
     "<c-e><c-s>",
-    [[python -c "import sys; print(sys.executable)"<enter>]]
+    [[<esc>python -c "import sys; print(sys.executable)"<enter>]]
   )
-  vim.keymap.set({ "t" }, "<c-e><c-a>", [[.\.venv\Scripts\activate<enter>]])
-  vim.keymap.set({ "t" }, "<c-e><c-d>", [[deactivate<enter>]])
+  vim.keymap.set({ "t" }, "<c-e><c-a>", [[<esc>.\.venv\Scripts\activate<enter>]])
+  vim.keymap.set({ "t" }, "<c-e><c-d>", [[<esc>deactivate<enter>]])
 else
   vim.keymap.set(
     { "t" },
     "<c-e><c-s>",
-    [[python3 -c "import sys; print(sys.executable)"<enter>]]
+    [[<c-u>python3 -c "import sys; print(sys.executable)"<enter>]]
   )
-  vim.keymap.set({ "t" }, "<c-e><c-a>", [[./.venv/bin/activate<enter>]])
-  vim.keymap.set({ "t" }, "<c-e><c-d>", [[deactivate<enter>]])
+  vim.keymap.set({ "t" }, "<c-e><c-a>", [[<c-u>./.venv/bin/activate<enter>]])
+  vim.keymap.set({ "t" }, "<c-e><c-d>", [[<c-u>deactivate<enter>]])
 end
 
 local terms = {}
