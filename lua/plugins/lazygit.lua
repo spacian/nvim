2f21c4c0133a11dfe855344735dd23fcbf3a88fd
Robert Markowski <robert.markowski123@gmail.com>
2026-08-18 13:38:37 +0200

    feat(plugins): lazygit - switch to more up to date fork

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,10 +1,15 @@
 return {
   {
-    "kdheepak/lazygit.nvim",
+    "103sbavert/lazygit.nvim",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      vim.g.lazygit_floating_window_scaling_factor = 1.0
+      require("lazygit").setup({
+        floating_window = {
+          scaling_factor = 1.0,
+          use_plenary = true,
+        },
+      })
       vim.keymap.set({ "n" }, "<leader>gg", function()
         vim.cmd("LazyGit")
       end)
@@ -14,14 +19,6 @@ return {
       vim.keymap.set({ "n" }, "<leader>gf", function()
         vim.cmd("LazyGitFilterCurrentFile")
       end)
-      vim.api.nvim_create_autocmd({ "BufEnter" }, {
-        callback = function()
-          if not BufIsSpecial() then
-            require("lazygit.utils").project_root_dir()
-          end
-        end,
-      })
-      vim.g.lazygit_floating_window_use_plenary = 0
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "lazygit",
         callback = function(args)
